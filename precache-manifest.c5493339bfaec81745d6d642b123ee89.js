1edb91f48ec01850fb8bd4b998804c8d3bf548a9
jared jacobsen <jaredjacobsen@pop-os.localdomain>
2020-08-16 23:26:00 +0700

    Updates

---
+++ precache-manifest.c5493339bfaec81745d6d642b123ee89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd04f95d95f921ec7573b09ca7414c1",
+    "url": "/flash/index.html"
+  },
+  {
+    "revision": "8d9876de6f0e26a06679",
+    "url": "/flash/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fd57358ab7ffd36a033a",
+    "url": "/flash/static/css/main.8a421087.chunk.css"
+  },
+  {
+    "revision": "8d9876de6f0e26a06679",
+    "url": "/flash/static/js/2.0a384568.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/flash/static/js/2.0a384568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd57358ab7ffd36a033a",
+    "url": "/flash/static/js/main.73c6cd17.chunk.js"
+  },
+  {
+    "revision": "081623a876e264324dc6",
+    "url": "/flash/static/js/runtime-main.655b1747.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flash/static/media/logo.5d5d9eef.svg"
+  }
+]);
